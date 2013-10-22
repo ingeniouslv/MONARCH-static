43b28656c447f61df8a29499efc49868f6729706
ingeniouslv <herb@ingeniouslv.com>
2013-10-22 11:36:18 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(window).load(function() {
+	$('.container-fluid').animate({ 'opacity': 1 }, 800);
+});
+
+$(function() {
+	var winH = $(window).height();
+	var winW = $(window).width();
+	$('.row').css({ 'height': winH, 'width': winW });
+	
+	if( (winW/winH) > (1280/720) ) {
+		$('.bgvid').css({ 
+			'height': winW/(1280/720), 
+			'width': winW
+		});
+	}else{
+		$('.bgvid').css({ 
+			'width': winH*(1280/720), 
+			'height': winH 
+		});
+	}
+});
+
+$(window).resize(function() {
+	var winH = $(window).height();
+	var winW = $(window).width();
+	$('.row').css({ 'height': winH, 'width': winW });
+	
+	if( (winW/winH) > (1280/720) ) {
+		$('.bgvid').css({ 
+			'height': winW/(1280/720), 
+			'width': winW
+		});
+	}else{
+		$('.bgvid').css({ 
+			'width': winH*(1280/720), 
+			'height': winH 
+		});
+	}
+});
+
